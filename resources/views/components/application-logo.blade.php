b0ca5f48ea92636bdb70bcd6a49f4d7236780374
Rodolfo Rohatan <bassilmatteo22@gmail.com>
2025-11-13 16:09:05 -0500

    se agrego logo de fenalco a la vista participan dashboard

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img {{ $attributes }} src="{{ asset('images/logo.svg') }}" alt="Logo de Mi Empresa">
+<img {{ $attributes }} src="{{ asset('images/logo-fenalco.png') }}" alt="Logo de Mi Empresa" class="w-64 h-auto">
