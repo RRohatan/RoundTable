d3e4b4c8e8b6098b90fa95236e2be774d15a985d
Rodolfo Rohatan <bassilmatteo22@gmail.com>
2025-11-20 19:41:13 -0500

    se hizo actualizacion al formulario de inscripcion

--- resources/views/public/event-registration.blade.php
+++ resources/views/public/event-registration.blade.php
@@ -88,11 +88,11 @@
     <div class="mt-2 space-y-2">
         <div class="flex items-center">
             <input id="role_buyer" name="role" type="radio" value="buyer" {{ old('role') == 'buyer' ? 'checked' : '' }} required class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
-            <label for="role_buyer" class="ml-3 block text-sm font-medium text-gray-700">Demandante (Busco productos/servicios)</label>
+            <label for="role_buyer" class="ml-3 block text-sm font-medium text-gray-700">Comprador (Busco productos/servicios)</label>
         </div>
         <div class="flex items-center">
             <input id="role_supplier" name="role" type="radio" value="supplier" {{ old('role') == 'supplier' ? 'checked' : '' }} required class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
-            <label for="role_supplier" class="ml-3 block text-sm font-medium text-gray-700">Oferente (Ofrezco productos/servicios)</label>
+            <label for="role_supplier" class="ml-3 block text-sm font-medium text-gray-700">Vendedor (Ofrezco productos/servicios)</label>
         </div>
     </div>
 </div>
